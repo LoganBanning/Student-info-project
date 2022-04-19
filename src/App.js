c8eca132009f78ba681a98131a2cd631f6e9005f
Logan Banning <loganboganb@gmail.com>
2022-04-18 21:10:51 -0600

    working on step 4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import StudentInfo from './Components/studentInfo';
 const FetchData = (props) => {
   const [unfilteredList, setUnfilteredList] = useState([]);
   const [search, setSearch] = useState("");
+  const [tagSearch, setTagSearch] = useState("");
   const [filteredList, setFilteredList] = useState([]);
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
     try {
@@ -18,24 +20,64 @@ const FetchData = (props) => {
     }
   }, []);
 
-  const updateFilteredList = (searchTerm) => {
-    const searchedStudent = unfilteredList.filter((student) => {
-      return (`${student.firstName} ${student.lastName}`).toLowerCase().includes(searchTerm.toLowerCase())
-    });
+  const updateFilteredList = (searchTerm, searchByTags) => {
+    if (searchByTags) {
+      setTagSearch(searchTerm);
+    } else {
+      setSearch(searchTerm);
+    }
+      // create a list of student ids that match the tag
+      const idsThatMatchTag = tags.map(tagObject => {
+        if(tagObject.tag.toLowerCase().includes(tagSearch.toLowerCase())) {
+          return tagObject.studentId;
+        }
+      });
+
+      // filter the student list, only include students whose id is in the list above
+      const searchedStudentsByTag = unfilteredList.filter((student) => {
+        return idsThatMatchTag.includes(student.id)
+      });
+
+      // filter the original list of students by a combinations of their first and last name
+      const searchedStudentsByName = unfilteredList.filter((student) => {
+        return (`${student.firstName} ${student.lastName}`).toLowerCase().includes(search.toLowerCase())
+      });
+
+      let finalList = [];
+      if(searchByTags) { // meaning the input of the tag-search bar has changed
+        finalList = searchedStudentsByTag
+        if(search.length > 0) {
+          finalList = [...finalList, searchedStudentsByName]
+        }
+        
+      } else { // meaning the input of the name search bar has changed
+        finalList = searchedStudentsByName
+        if(tagSearch.length > 0) {
+          finalList = [...finalList, searchedStudentsByTag];
+        }
+      }
+    
     // setFilteredList to the new list
-    setFilteredList(searchedStudent);
-    setSearch(searchTerm);
+    setFilteredList(finalList);
   }
 
   const studentMap = (studentList) => {
     return studentList.map((student) => {
+      const studentTags = tags.filter((tag) => {
+        return student.id === tag.studentId
+      })
       return (
-          <StudentInfo student={student} />
+          <StudentInfo key={student.id} tags={studentTags} addTag={addTag} student={student} />
       );
     });
   };
 
-
+  const addTag = (tag, studentId) => {
+   // Add the tag and the studentId in a object to the tags list and update the state
+   const newTag = {tag, studentId};
+   const newTags = [...tags, newTag];
+   setTags(newTags);
+  }
 
   return (
     <div className="student-info-page font-link">
@@ -44,12 +86,19 @@ const FetchData = (props) => {
           className="search-input"
           type="search"
           id="search"
-          placeholder="Search by name"
-          onChange={(e) => updateFilteredList(e.target.value)}
+          placeholder="Search by Name"
+          onChange={(e) => updateFilteredList(e.target.value, false)}
+        ></input>
+        <input
+          className="search-input"
+          type="search"
+          id="search-tag"
+          placeholder="Search by Tag"
+          onChange={(e) => updateFilteredList(e.target.value, true)}
         ></input>
         <div className="scroll-and-containers">
-          {search.length > 0 && studentMap(filteredList)}
-          {search.length === 0 && studentMap(unfilteredList)}
+          {(search.length > 0 || tagSearch.length > 0) && studentMap(filteredList)}
+          {(search.length === 0 && tagSearch.length === 0) && studentMap(unfilteredList)}
         </div>
       </div>
     </div>
--- src/Components/studentInfo.js
+++ src/Components/studentInfo.js
@@ -1,24 +1,39 @@
 import { useState } from 'react';
 
   const StudentInfo = (props) => {
-  console.log('StudentInfo Props', props);
   const [areGradesOpen, setAreGradesOpen] = useState(false);
-  const { student } = props
+  const [tagInput, setTagInput] = useState('');
+  const { student, addTag, tags } = props
   const { id, firstName, lastName, email, skill, grades, pic, company } =
   student;
 
   const allGrades = (grades) => {
-    let gradesArr = [];
-    for(let i = 0; i < grades.length; i++){
-      let result = grades[i];
-      result.push(gradesArr);
-    }
-    return gradesArr;
-  }
+     return grades.map((grade, index) => {
+      return (
+        <div key={index}>Test {index + 1}: {grade}%</div>
+      )
+    })
+  };
+
+  const allTags = (tags) => {
+    return tags.map((tagObject, index) => {
+     return (
+       <div key={index}>{tagObject.tag}</div>
+     )
+   })
+ };
 
   const toggleGrades = () => {
     setAreGradesOpen(!areGradesOpen);
   };
+
+  const submitTag = (e) => {
+      e.preventDefault()
+      // add the tag
+      addTag(tagInput, id);
+      // clear the input
+      setTagInput('');
+  }
 
   const average = (grades) => {
     let total = 0;
@@ -34,11 +49,16 @@ import { useState } from 'react';
     <div className="flex-row">
       <img src={pic} className="student-img" />
       <div className="student-info">
-        <div className="student-name">{`${firstName.toUpperCase()} ${lastName.toUpperCase()}`}</div>
+        <div className="student-name">{`${firstName?.toUpperCase()} ${lastName?.toUpperCase()}`}</div>
         <div>Email: {email}</div>
         <div>Company: {company}</div>
         <div>Skill: {skill}</div>
         <div>Average: {average(grades)}</div>
+        {allTags(tags)}
+        <form onSubmit={submitTag}>
+            <input onChange={(e) => setTagInput(e.target.value)} type='text' value={tagInput}></input>
+        </form>
+        {areGradesOpen && allGrades(grades)}
       </div>
     </div>
     <button onClick={toggleGrades} className='toggle-grades-btn'>+</button>
